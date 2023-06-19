7c05b1c5682561b707396b134307b4db022b9116
omarlopezoficial <contacto.omarlopez@gmail.com>
2023-06-19 16:13:04 -0400

    Se añadio la primera fase del paquete captura de datos

--- lib/presentation/screens/ocr-audio/ocrAudio_screen.dart
+++ lib/presentation/screens/ocr-audio/ocrAudio_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:note_app_frontend/config/theme/app_theme.dart';
 import 'package:note_app_frontend/presentation/widgets/shared/appBarMenu.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class OcrAudioScreen extends StatefulWidget {
   const OcrAudioScreen({super.key});
@@ -13,6 +15,44 @@ class _OcrAudioScreenState extends State<OcrAudioScreen> {
 
   bool openCapture = false;
 
+  SpeechToText _speechToText = SpeechToText();
+  bool _speechEnabled = false;
+  String _lastWords = '';
+
+@override
+  void initState() {
+    super.initState();
+    _initSpeech();
+  }
+
+  /// This has to happen only once per app
+  void _initSpeech() async {
+    _speechEnabled = await _speechToText.initialize();
+    setState(() {});
+  }
+
+  /// Each time to start a speech recognition session
+  void _startListening() async {
+    await _speechToText.listen(onResult: _onSpeechResult);
+    setState(() {});
+  }
+
+  /// Manually stop the active speech recognition session
+  /// Note that there are also timeouts that each platform enforces
+  /// and the SpeechToText plugin supports setting timeouts on the
+  /// listen method.
+  void _stopListening() async {
+    await _speechToText.stop();
+    setState(() {});
+  }
+
+  /// This is the callback that the SpeechToText plugin calls when
+  /// the platform returns recognized words.
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      _lastWords = result.recognizedWords;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +66,7 @@ class _OcrAudioScreenState extends State<OcrAudioScreen> {
            const SizedBox(height: 180),
           Image.asset("assets/audioCollector.png",width: 200,),
           const SizedBox(height: 20),
-          const Text('Presiona Play',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
+          const Text('Presiona el microfono',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
           const SizedBox(height: 10),
           const Text('Para comenzar a recolectar texto por audio')
       ],)
@@ -38,6 +78,18 @@ class _OcrAudioScreenState extends State<OcrAudioScreen> {
           Image.asset("assets/record_2.gif",width: 200,),
           const SizedBox(height: 20),
           const Text('Grabando...',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
+          Text(
+                  // If listening is active show the recognized words
+                  _speechToText.isListening
+                      ? '_lastWords'
+                      // If listening isn't active but could be tell the user
+                      // how to start it, otherwise indicate that speech
+                      // recognition is not yet ready or not supported on
+                      // the target device
+                      : _speechEnabled
+                          ? 'Precione pausa o stop para detener el microfono...'
+                          : 'El microfono no esta habilitado',
+                ),
 
       ],)
       
@@ -47,11 +99,14 @@ class _OcrAudioScreenState extends State<OcrAudioScreen> {
      floatingActionButton: ( !openCapture ) ? 
           fabButton(
           onPressed: (){
-            openCapture = true;
-            setState(() {});
+
+            _speechToText.isNotListening ? _startListening : _stopListening;
+             openCapture = true;
+             setState(() {});
+
           },
           tagName: "Play",   
-          icon: Icons.play_arrow, 
+          icon: _speechToText.isNotListening ? Icons.mic_off : Icons.mic, 
            bgColor: AppTheme.primary,
          )
            :  
