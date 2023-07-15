b1ec3e2c81ae0ecba9017d7c9898ff0d869ab5ab
omarlopezoficial <contacto.omarlopez@gmail.com>
2023-07-15 08:29:58 -0400

    Home Style Fix

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:note_app_frontend/presentation/screens/note/noteList_screen.dart';
-import 'package:note_app_frontend/presentation/screens/ocr-audio/ocrAudio_screen.dart';
-import 'package:note_app_frontend/presentation/widgets/shared/sidebar_menu.dart';
+import 'package:note_app_frontend/presentation/widgets/shared/appBarMenu.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
 
 import '../../../config/theme/app_theme.dart';
 import '../../widgets/home/menu_cards.dart';
+import '../../widgets/home/menu_item.dart';
+import '../../widgets/note/createNoteFAB_widget.dart';
 import '../../widgets/ocrcam/modal_optioncam.dart';
-import '../../widgets/shared/appBarMenu.dart';
+import '../../widgets/shared/sidebar_menu.dart';
 import '../note/noteEditor_screen.dart';
-
+import '../note/noteList_screen.dart';
+import '../ocr-audio/ocrAudio_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -20,172 +22,97 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      backgroundColor: AppTheme.bgGray,
-      drawer: const SideBar(),
-      appBar: AppBarMenu(context),
-      body: 
-              SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 0,vertical: 10),
-                  child: Column(
-                    children: const [
+    return Scaffold(
+      appBar:                       AppBarMenu(context),
+      drawer:                       const SideBar(),
+      body:                         const SafeArea(child: BodyContent()) ,
+      floatingActionButton:         const CreateNoteFAB(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,      
+    );
+  }
+}
 
-                      // Primera sección : Menu de opciones
-                       Align(
-                        alignment: Alignment.center,
-                        child: Text('Opciones de Menú',textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.w500,fontSize: 18),),
-                      ),                   
-                       Text('El poder de la I.A. en tus manos'),
-                       SizedBox(height: 20,),
-                
-                
-                       MenuOptions(),
+class BodyContent extends StatelessWidget {
+  const BodyContent({
+    super.key,
+  });
 
-                      // Primera sección : Últimas 3 notas
-                       SizedBox(height: 20,),
-                       Align(
-                        alignment: Alignment.center,
-                        child: Text('Últimas Notas',textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.w500,fontSize: 18),),
-                      ),                   
-                       Text('Continua el trabajo donde lo dejaste'),
-                       SizedBox(height: 20,),
-
-
-                     LastNotes(),
-
-
-                    SizedBox(height: 50,),
-
-                      
-
-
-
-                    ],
-                  ),
-                ),
-              )
-           ,
-       floatingActionButton: 
-         
-          FloatingActionButton(
-            heroTag: "Create",
-            shape: const StadiumBorder(),
-            backgroundColor: AppTheme.primary,
-            onPressed: () {
-              final route = MaterialPageRoute(
-                builder: (context) => NoteEditorScreen(),
-              );
-              Navigator.pushReplacement(context, route);
-            },
-            child: const Icon(Icons.add,color: AppTheme.text_dark),
+  @override
+  Widget build(BuildContext context) {
+    return Padding( 
+      padding: const EdgeInsets.symmetric(horizontal: 18,vertical: 10) ,  
+      child:      
+        Column( 
+          mainAxisSize: MainAxisSize.min,
+          children: 
+         [
+           const SizedBox(
+            height: 10,
+          ),
+          //const Text('Explora el poder de la',textAlign: TextAlign.left, style: TextStyle(fontWeight: FontWeight.w300,fontSize: 32)),
+         // const Text('Inteligencia Artificial',textAlign: TextAlign.left, style: TextStyle(fontWeight: FontWeight.w500,fontSize: 32,)),
+          
+          GradientText(
+    'Explora                   ',
+    style: const TextStyle(
+        fontSize: 46.0,
+    ),
+    colors: const [
+             Color.fromARGB(255, 255, 148, 17),
+             Color.fromARGB(255, 255, 216, 21),
+             Color.fromARGB(255, 255, 181, 21),
+             Color.fromARGB(255, 222, 126, 1),
+    ],
+),
+          const SizedBox(
+            height: 20,
           ),
 
+          Row( 
+             crossAxisAlignment: CrossAxisAlignment.center,
+            children:  [ 
+               Expanded(
+                child: 
+                HomeItem(title: 'Lista de Notas     ',    bodyText: 'Cree, edite o elimine sus notas', color: AppTheme.note_1, icon: Icons.edit_note,               onPressed: () {final route = MaterialPageRoute(builder: (context) => const NoteListScreen());  Navigator.pushReplacement(context, route); },),     
+              ),
+                Container(
+                  width: 10,
+                ),
 
-       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+                Expanded(
+                child: 
+                HomeItem(title: 'Redactor de audio     ',    bodyText: '¡Convierta un audio en texto!', color: AppTheme.note_1, icon: Icons.edit_note,               onPressed: () {final route = MaterialPageRoute(builder: (context) => const OcrAudioScreen());  Navigator.pushReplacement(context, route); },),   
+              ),
+           
+           
+          ], ),
+
+
+          const SizedBox(
+            height: 10,
+          ),
+         
+           HomeItem(title: 'Lector de imagenes     ',    bodyText: '¡Convierta una imagen en texto!', color: AppTheme.note_1, icon: Icons.edit_note,               onPressed: () { optionOcrCam(context); },),  
+
+          const SizedBox(
+            height: 30,
+          ),  
+              
+          
+          const Text('Tu última nota                                                     ' , style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18,)),
+
+           const SizedBox(
+            height: 10,
+          ),  
+
+            HomeItem(title: 'Lista de Notas     ',    bodyText: 'Cree, edite o elimine sus notas', color: AppTheme.note_1, icon: Icons.edit_note,               onPressed: () {final route = MaterialPageRoute(builder: (context) => const NoteListScreen());  Navigator.pushReplacement(context, route); },),      
+           
+        ],
+    )
     );
   }
 }
 
-class LastNotes extends StatelessWidget {
-  const LastNotes({
-    super.key,
-  });
 
-  @override
-  Widget build(BuildContext context) {
-    return ListView.separated(
-      //padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
-      itemBuilder: (BuildContext context, int index) {
-        return const NoteItem() ;
-      }, 
-      separatorBuilder: (BuildContext context, int index) => const Divider(thickness: 5,), 
-      itemCount: 2);
-  }
-}
 
-class NoteItem extends StatelessWidget {
-  const NoteItem({
-    super.key,
-  });
 
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.topLeft,
-      child: MaterialButton(   
-        onPressed: () {  },
-        child: Column(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-         Text('Fecha Nota', textAlign: TextAlign.left ,style: TextStyle(fontSize: 10),),
-         Text('Titulo Nota',textAlign: TextAlign.left , style: TextStyle(fontSize: 16 , fontWeight: FontWeight.bold),),
-         Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a massa at sapien.',textAlign: TextAlign.left, style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),),
-         Text('Etiqueta',textAlign: TextAlign.left, style: TextStyle(fontSize: 14,color: AppTheme.note_3),),
-        
-        
-        ],), )
-    );
-  }
-}
-
-class MenuOptions extends StatelessWidget {
-  const MenuOptions({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return 
-
-GridView.count(
-                  crossAxisCount: 2,
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  childAspectRatio: 1.30,
-                  crossAxisSpacing: 0,
-                  mainAxisSpacing:  20,
-                  children:   [
-                     MenuItem( title: 'Lista de Notas     ',    bodyText: 'Cree, edite o elimine sus notas', color: AppTheme.note_1, icon: Icons.edit_note,               onPressed: () {final route = MaterialPageRoute(builder: (context) => const NoteListScreen());  Navigator.pushReplacement(context, route); },),
-                     MenuItem( title: 'Lector de imagenes  ',bodyText: '¡Convierta una imagen en texto!', color: AppTheme.note_2, icon: Icons.photo_camera,               onPressed: () { optionOcrCam(context); },),
-                     MenuItem( title: 'Redactor de audio   ', bodyText: '¡Convierta un audio en texto!',   color: AppTheme.note_3, icon: Icons.multitrack_audio_outlined, onPressed: () {final route = MaterialPageRoute(builder: (context) => const OcrAudioScreen());  Navigator.pushReplacement(context, route); },),
-                    
-                    ]
-                  );
-
-      // Text('Opciones de Menú',textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.w500,fontSize: 18),),
-  }
-}
-
-class MenuItem extends StatelessWidget {
-  
-  final VoidCallback onPressed;
-  final String title;
-  final IconData   icon;
-  final Color  color;
-  final String bodyText;
-  
-  
-  const MenuItem({
-    super.key, required this.title, required this.icon, required this.color, required this.bodyText, required this.onPressed,
-  });
-
-  
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialButton(
-      onPressed: onPressed,
-
-      child:  OptionCard(
-        title: title, 
-        theme: color,
-        icono: icon,
-        body: bodyText
-      ),
-    );
-  }
-}
