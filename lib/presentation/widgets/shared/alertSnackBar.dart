4014bf87aed4a950cd2fc1f66b9043de1248ac86
Ashly <avct2001@gmail.com>
2023-07-17 20:32:32 -0400

    Alertas

---
+++ lib/presentation/widgets/shared/alertSnackBar.dart
@@ -0,0 +1,18 @@
+  import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
+import 'package:flutter/material.dart';
+
+SnackBar AlertSnackBar({required String titulo, required String mensaje, required ContentType tipo}) {
+
+    final snackBar = SnackBar(
+      elevation: 0,
+      behavior: SnackBarBehavior.floating,
+      backgroundColor: Colors.transparent,
+      content: AwesomeSnackbarContent(
+        title: titulo,
+        message: mensaje,
+    
+        contentType: tipo,
+      ),
+    );
+    return snackBar;
+  }
